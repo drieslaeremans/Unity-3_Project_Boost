3d54f07810d798c778b8443e714d21a068ca63af
Dries Laeremans <drieslaeremans@hotmail.com>
2019-02-10 21:46:08 +0100

    (48+49) 12_PB Resolving Movement Bugs + Using SerializedField vs public

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 200f;
+    [SerializeField] float mainThrust = 800f;
+
     private new Rigidbody rigidbody;
     AudioSource boostAudioSource;
-    bool boostPlay;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         boostAudioSource = GetComponent<AudioSource>();
-        boostPlay = false;
     }
 
     // Update is called once per frame
@@ -25,36 +26,18 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        HandleBoostInput();
+        ThrustInput();
         HandleRotationInput();
 
     }
 
-    private void HandleRotationInput()
-    {
-        /* 
-                 * Rotate around Z-axis (forward) if specified button is pressed
-                 * Rotation is set at 30 degrees per second
-                 * Vector3.forward * Time.deltaTime = 1 degree per second
-                 * Time.deltaTime => amount of seconds the previous frame took to render, 
-                 * using this makes sure speed op rotation stays equal on all framerates
-                 */
-        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate((Vector3.forward * Time.deltaTime) * 30);
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate((-Vector3.forward * Time.deltaTime) * 30);
-        }
-    }
-
-    private void HandleBoostInput()
+    private void ThrustInput()
     {
         // Boost Forward if space, Z or Arrow Up are pressed
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            float thrustThisFrame = mainThrust * Time.deltaTime;
+            rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
 
             if (!boostAudioSource.isPlaying)
             {
@@ -64,4 +47,35 @@ public class Rocket : MonoBehaviour
         else
             boostAudioSource.Stop();
     }
+
+    private void HandleRotationInput()
+    {
+        // Take manual control of rotation
+        rigidbody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        /* 
+                 * Rotate around Z-axis (forward) if specified button is pressed
+                 * Rotation is set at 30 degrees per second
+                 * Vector3.forward * Time.deltaTime = 1 degree per second
+                 * Time.deltaTime => amount of seconds the previous frame took to render, 
+                 * using this makes sure speed op rotation stays equal on all framerates
+                 */
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.Rotate((Vector3.forward * Time.deltaTime) * 30);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.Rotate((-Vector3.forward * Time.deltaTime) * 30);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        // resume fysics control of rotation
+        rigidbody.freezeRotation = false;
+    }
+
+    
 }
