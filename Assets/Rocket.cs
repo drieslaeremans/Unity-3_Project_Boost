29ef1ed5df9cf92afbc66838743ba689f05df4ea
Dries Laeremans <drieslaeremans@hotmail.com>
2019-02-10 19:48:17 +0100

    (45) 11_PB Using Time.deltaTime

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +21,26 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if(Input.GetKey(KeyCode.Space))
+        // Boost Forward if space, Z or Arrow Up are pressed
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z) ) 
         {
             rigidbody.AddRelativeForce(Vector3.up);
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        /* 
+         * Rotate around Z-axis (forward) if specified button is pressed
+         * Rotation is set at 30 degrees per second
+         * Vector3.forward * Time.deltaTime = 1 degree per second
+         * Time.deltaTime => amount of seconds the previous frame took to render, 
+         * using this makes sure speed op rotation stays equal on all framerates
+         */
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftArrow) )
         {
-            print("Rotating left");
+            transform.Rotate((Vector3.forward * Time.deltaTime) * 30);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) )
         {
-            print("Rotating right");
+            transform.Rotate((-Vector3.forward * Time.deltaTime) * 30);
         }
 
     }
